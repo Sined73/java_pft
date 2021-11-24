fe2cf3741fd0d909e4bf00844b75d2f33e057e68
Sined73 <dakateye@yandex.ru>
2021-11-24 14:56:27 +0400

    Доработка ДЗ №2

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -5,12 +5,25 @@ public class Point {
   public double x;
   public double y;
 
-  public Point(double x, double y) { //Конструктор. Закомитить, если значения будут вписаны в тело
+  public Point(double x, double y) {
     this.x = x;
     this.y = y;
   }
 
-  public double distance(Point p2) {          // Метод. Закоммитить, если метод нужно отключить
-    return Math.sqrt((p2.x - this.x)*(p2.x - this.x) + (p2.y - this.y)*(p2.y - this.y));
+  public double distance(Point p2) {
+    return Math.sqrt((p2.x - this.x) * (p2.x - this.x) + (p2.y - this.y) * (p2.y - this.y));
+  }
+
+  public static double distance(Point p1, Point p2) {
+    return Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
+  }
+}
+
+class PointStart {
+  public static void main(String[] args) {
+    Point p1 = new Point(12, -13);
+    Point p2 = new Point(-11, 100);
+    System.out.println("Расстояние между двумя точками равно " + Point.distance(p1, p2) );
+    System.out.println("Расстояние между двумя точками равно " + p1.distance(p2) );
   }
 }
