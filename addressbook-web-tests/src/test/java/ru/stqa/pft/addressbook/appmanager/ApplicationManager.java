e7585ee66feb74a9ec00aeb475f4f83210f037bc
Sined73 <dakateye@yandex.ru>
2021-12-04 19:02:08 +0400

    Сравнение объектов при помощи оператора == и метода equals

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -21,11 +21,11 @@ public class ApplicationManager {
   }
 
   public void init() {
-    if (browser == BrowserType.CHROME) {
+    if (browser.equals(BrowserType.CHROME)) {
       wd = new ChromeDriver();
-    } else if (browser == BrowserType.FIREFOX) {
+    } else if (browser.equals(BrowserType.FIREFOX)) {
       wd = new FirefoxDriver();
-    } else if (browser == BrowserType.EDGE) {
+    } else if (browser.equals(BrowserType.EDGE)) {
       wd = new EdgeDriver();
     }
 //    wd = new ChromeDriver();
