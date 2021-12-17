400f6e403683c4425a144a32456cd8e6ab4c4c12
Sined73 <dakateye@yandex.ru>
2021-12-17 23:44:42 +0400

    Кол-во элементов в контактах

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -1,20 +1,26 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
-public class ContactModificationTests extends TestBase{
+public class ContactModificationTests extends TestBase {
 
   @Test
   public void testContactModification() {
-    if (! app.getNavigationHelper().isThereAModButton()) {
-      app.getContactHelper().createContact(new ContactData("Denis", "Kateev", "Moscow city", "+79111111111", "mail@mail.ru", "Test1"), true);
+    int before = app.getContactHelper().getContactCount();
+    if (!app.getNavigationHelper().isThereAModButton()) {
+      app.getContactHelper().createContact(new ContactData("Denis", "Kateev", "Moscow city",
+              "+79111111111", "mail@mail.ru", "Test1"), true);
       app.getNavigationHelper().goToHomePage();
     }
     app.getNavigationHelper().gotoModificateContact();
-    app.getContactHelper().fillContactForm(new ContactData("Алёша", "Попович", "Ростов", "+79111155112", "mail2@gmail.ru", null), false);
+    app.getContactHelper().fillContactForm(new ContactData("Алёша", "Попович", "Ростов",
+            "+79111155112", "mail2@gmail.ru", "[none]"), false);
     app.getContactHelper().submitContactModification();
     app.getNavigationHelper().goToHomePage();
+    int after = app.getContactHelper().getContactCount();
+    Assert.assertEquals(after, before);
     app.getSessionHelper().logout();
   }
 }
