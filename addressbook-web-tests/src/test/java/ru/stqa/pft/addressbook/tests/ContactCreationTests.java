e04600765ddc359b2a9d52ac0d734593eb949666
Sined73 <dakateye@yandex.ru>
2022-01-05 21:18:02 +0400

    6.5. Загрузка тестовых данных из файла

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -5,7 +5,10 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -16,20 +19,16 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class ContactCreationTests extends TestBase {
 
   @DataProvider
-  public Iterator<Object[]> validContacts() {
+  public Iterator<Object[]> validContacts() throws IOException {
     List<Object[]> list = new ArrayList<Object[]>();
-    File photo = new File("src/test/resources/panda.png");
-    File photo2 = new File("src/test/resources/mouse.png");
-    File photo3 = new File("src/test/resources/cat.png");
-    list.add(new Object[] {new ContactData().withFirstname("Alexey").withLastname("Katuev").withAddress("Санкт-Петербург," +
-                    "пр-т Невский, д. 15, корп.15/4, кв. 2032").withMobilePhone("+79111111111")
-            .withEmail("mail@mail.ru").withPhoto(photo)});
-    list.add(new Object[] {new ContactData().withFirstname("Николай").withLastname("Басков").withAddress("Москва," +
-                    "ул. Арбат , д. 15, корп.23, кв. 236").withMobilePhone("+79159563257")
-            .withEmail("mail2@mail.ru").withPhoto(photo2)});
-    list.add(new Object[] {new ContactData().withFirstname("Семен").withLastname("Горбунков").withAddress("Черноморск," +
-                    "ул. Морская , д. 21, кв. 9").withMobilePhone("88264957496")
-            .withEmail("mail2084@mail.ru").withPhoto(photo3)});
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.csv")));
+    String line = reader.readLine();
+    while (line != null) {
+      String[] split = line.split(";");
+      list.add(new Object[] {new ContactData().withFirstname(split[0]).withLastname(split[1]).withAddress(split[2])
+              .withEmail(split[3]).withMobilePhone(split[4])});
+      line = reader.readLine();
+    }
     return list.iterator();
   }
 
