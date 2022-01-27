cf81fcb32d5a9c0e6a5a9cedc522932c5e3b3f6a
Sined73 <dakateye@yandex.ru>
2022-01-27 19:51:14 +0400

    ДЗ 20

--- rest-sample/src/test/java/ru/stqa/pft/rest/RestTests.java
+++ rest-sample/src/test/java/ru/stqa/pft/rest/RestTests.java
@@ -14,7 +14,7 @@ import java.util.Set;
 
 import static org.testng.Assert.assertEquals;
 
-public class RestTests {
+public class RestTests extends TestBase {
 
   @Test
   public void testCreateIssue() throws IOException {
@@ -27,23 +27,28 @@ public class RestTests {
     oldIssues.add((newIssue.withId(issueId)));
     assertEquals(newIssues, oldIssues);
   }
-
   private Set<Issue> getIssues() throws IOException {
     String json = getExecutor().execute(Request.Get("https://bugify.stqa.ru/api/issues.json")).returnContent().asString();
     JsonElement parsed = JsonParser.parseString(json);
     JsonElement issues = parsed.getAsJsonObject().get("issues");
-    return new Gson().fromJson(issues, new TypeToken<Set<Issue>>() {}.getType());
+    return new Gson().fromJson(issues, new TypeToken<Set<Issue>>() {
+    }.getType());
   }
 
   private Executor getExecutor() {
     return Executor.newInstance().auth("288f44776e7bec4bf44fdfeb1e646490", "");
   }
-
   private int createIssue(Issue newIssue) throws IOException {
     String json = getExecutor().execute(Request.Post("https://bugify.stqa.ru/api/issues.json")
             .bodyForm(new BasicNameValuePair("subject", newIssue.getSubject()),
                     new BasicNameValuePair("description", newIssue.getDescription()))).returnContent().asString();
     JsonElement parsed = JsonParser.parseString(json);
     return parsed.getAsJsonObject().get("issue_id").getAsInt();
+  }
+
+  @Test
+  public void testPrint() throws IOException {
+    skipIfNotFixed(1697); //1697 "Closed", 1698 "Resolved", 1699 "In Progress", 1700 "Open"
+    System.out.println("Багрепорт решен либо закрыт");
   }
 }
